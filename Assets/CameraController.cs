65c635d3338232dbd2c801ba819a614aa6c41a1d
VERLYN M. FISCHER <verlyn.fischer@us.ibm.com>
2016-04-23 16:54:44 -0500

    First Draft
    Basic building blocks

---
+++ Assets/CameraController.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject ship;
+	//public Text txt;
+
+	private bool firstperson;
+	//private Vector3 offset;
+	//private Vector3 offset2;
+	//private Vector3 toffset;
+	//private Vector3 toffset2;
+	private ConfigurableJoint cj;
+
+	// Use this for initialization
+	void Start () {
+		//offset = transform.position - ship.transform.position;
+		//offset2 = offset + new Vector3 (0, 2, -6);
+		//firstperson = true;
+		//cj = GetComponent<ConfigurableJoint>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		//Get Camera View
+		//float view = Input.GetAxis ("View");
+		//txt.text = "ShipX: " + ship.transform.position.x.ToString ();
+
+		// Set bool based on inputs
+		//if(view > 0.0f){
+		//	firstperson = true;
+		//}
+
+		//if(view < 0.0f){
+		//	firstperson = false;
+		//}
+
+		// Locate Camera
+
+		//toffset = ship.transform.InverseTransformDirection (offset);
+		//toffset2 = ship.transform.InverseTransformDirection (offset2);
+
+		//if (firstperson) {
+		//	transform.position = transform.TransformDirection (ship.transform.localPosition + toffset);
+		//}
+		//else {
+		//	transform.position = transform.TransformDirection (ship.transform.localPosition + toffset2);
+		//}
+
+		//if(firstperson){
+		//	cj.connectedAnchor.Set(0.0f, 5.0f, 4.0f);
+		//}
+		//else
+		//{	
+		//	cj.connectedAnchor.Set(0.0f, 2.0f, -6.0f);
+		//}
+
+		// Rotate Camera
+		transform.rotation = ship.transform.rotation;
+	}
+}
---
+++ Assets/JetControls.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class JetControls : MonoBehaviour {
+
+
+	public Text txt;
+
+
+	private Rigidbody rb;
+	private HingeJoint hj;
+
+	private float roll_value;
+	private float pitch_value;
+
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		//hj = GetComponent<HingeJoint> ();
+	
+		rb.AddRelativeForce (Vector3.forward*10,ForceMode.VelocityChange);
+		//roll_value = 0.0f; pitch_value = 0.0f;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		// Get joystick values
+
+		float moveRoll = Input.GetAxis ("Roll");
+		float movePitch = Input.GetAxis ("Pitch");
+		float moveThrust = Input.GetAxis ("Thrust");
+
+		// Nullify input if too small
+
+		if(Mathf.Abs(moveThrust)<0.01f){moveThrust = 0.0f;}
+
+		//Add or remove hinge
+		//if hinge exists
+			//if no movement - remove hinge
+			//else - adjust hinge parameters
+		//else
+			//if no movement - do nothing
+			//else - create hinge and set parameters
+
+		if(Mathf.Abs(moveRoll) < 0.01f){
+			roll_value = 0;}
+		else{
+			roll_value = 2 / moveRoll;}
+
+		if(Mathf.Abs(movePitch) < 0.01f){
+			pitch_value = 0;}
+		else{
+			pitch_value = 2 / movePitch;}
+
+		if (hj != null) {
+			if (roll_value == 0 && pitch_value == 0) {
+				Destroy(hj);
+			} else {
+				hj.anchor = new Vector3 (roll_value, pitch_value, 0);
+				hj.axis = Vector3.Normalize(new Vector3 (pitch_value, roll_value, 0));
+			}
+		} else {
+			if (roll_value == 0 && pitch_value == 0) {
+				//Do nothing
+			} else {
+				hj = gameObject.AddComponent<HingeJoint>();
+				hj.autoConfigureConnectedAnchor = true;
+				hj.anchor = new Vector3 (roll_value, pitch_value, 0);
+				hj.axis = Vector3.Normalize(new Vector3 (pitch_value, roll_value, 0));
+			}
+		}
+
+		//Position hinge location
+		//if(Mathf.Abs(moveRoll) < 0.01f){
+		//	roll_value = 2;}
+		//else{
+		//	roll_value = 2 / moveRoll;}
+
+		//if(Mathf.Abs(movePitch) < 0.01f){
+		//	pitch_value = 0;}
+		//else{
+		//	pitch_value = 2 / movePitch;}
+		//
+		//hj.anchor = new Vector3 (roll_value, pitch_value, 0);
+		//hj.axis = Vector3.Normalize(new Vector3 (pitch_value, roll_value, 0));
+
+
+		// Apply velocity change
+		rb.AddRelativeForce (new Vector3(0,0,moveThrust*10));
+
+		// View parameters
+
+		txt.text = "roll_Value: " + roll_value.ToString() + " Speed: "+ Vector3.Magnitude(rb.velocity).ToString();
+
+		// Check for application quit
+
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			Application.Quit();
+		}
+
+	}
+}

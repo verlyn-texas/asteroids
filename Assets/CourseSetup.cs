a413a327c22a26597608263a5aae710bf05a26e2
VERLYN M. FISCHER <verlyn.fischer@us.ibm.com>
2016-04-28 21:37:47 -0500

    PhysicsMostlyFixed
    Physics mostly in place for ship.  Have placed lots of asteroids in
    world.

---
+++ Assets/CourseSetup.cs
@@ -0,0 +1,143 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CourseSetup : MonoBehaviour {
+
+	public Transform myRock1;
+	public Transform myRock2;
+	public Transform myRock3;
+	public Transform myRock4;
+
+	private Transform thisRock;
+	private float xloc, yloc, zloc;
+	private float xvel, yvel, zvel;
+	private float xrot, yrot, zrot;
+	private float xtorq, ytorq, ztorq;
+	private Rigidbody rb;
+	private GameObject myRockGameObject;
+	private Vector3 locVector;
+	private Vector3 mytoDirection;
+	private Vector3 torqueVector;
+	private Vector3 forceVector;
+
+	// Use this for initialization
+	void Start () {
+		//myRock1
+		for (var x = 0; x < 1000; x++) {
+
+			xloc = Random.Range (-200, 1300);
+			yloc = Random.Range (-200, 200);
+			zloc = Random.Range (-200, 1700);
+			locVector = new Vector3 (xloc, yloc, zloc);
+			thisRock = (Transform)Instantiate (myRock1, locVector, Quaternion.identity);
+			rb = thisRock.GetComponent<Rigidbody> ();
+
+			xvel = Random.Range (-10, 10);
+			yvel = Random.Range (-10, 10);
+			zvel = Random.Range (-10, 10);
+			forceVector = new Vector3 (xvel, yvel, zvel);
+			rb.AddRelativeForce(forceVector,ForceMode.VelocityChange);
+
+			xrot = Random.Range (-10, 10);
+			yrot = Random.Range (-10, 10);
+			zrot = Random.Range (-10, 10);
+			mytoDirection = new Vector3 (xrot, yrot, zrot);
+			rb.rotation.SetFromToRotation (rb.transform.forward, mytoDirection);
+
+			xtorq = Random.Range (-2, 2);
+			ytorq = Random.Range (-2, 2);
+			ztorq = Random.Range (-2, 2);
+			torqueVector = new Vector3 (xtorq, ytorq, ztorq);
+			rb.AddRelativeTorque (torqueVector, ForceMode.VelocityChange);
+		}
+		//myRock2
+		for (var x = 0; x < 1000; x++) {
+
+			xloc = Random.Range (-200, 1300);
+			yloc = Random.Range (-200, 200);
+			zloc = Random.Range (-200, 1700);
+			locVector = new Vector3 (xloc, yloc, zloc);
+			thisRock = (Transform)Instantiate (myRock2, locVector, Quaternion.identity);
+			rb = thisRock.GetComponent<Rigidbody> ();
+
+			xvel = Random.Range (-10, 10);
+			yvel = Random.Range (-10, 10);
+			zvel = Random.Range (-10, 10);
+			forceVector = new Vector3 (xvel, yvel, zvel);
+			rb.AddRelativeForce(forceVector,ForceMode.VelocityChange);
+
+			xrot = Random.Range (-10, 10);
+			yrot = Random.Range (-10, 10);
+			zrot = Random.Range (-10, 10);
+			mytoDirection = new Vector3 (xrot, yrot, zrot);
+			rb.rotation.SetFromToRotation (rb.transform.forward, mytoDirection);
+
+			xtorq = Random.Range (-2, 2);
+			ytorq = Random.Range (-2, 2);
+			ztorq = Random.Range (-2, 2);
+			torqueVector = new Vector3 (xtorq, ytorq, ztorq);
+			rb.AddRelativeTorque (torqueVector, ForceMode.VelocityChange);
+		}
+		//myRock3
+		for (var x = 0; x < 1000; x++) {
+
+			xloc = Random.Range (-200, 1300);
+			yloc = Random.Range (-200, 200);
+			zloc = Random.Range (-200, 1700);
+			locVector = new Vector3 (xloc, yloc, zloc);
+			thisRock = (Transform)Instantiate (myRock3, locVector, Quaternion.identity);
+			rb = thisRock.GetComponent<Rigidbody> ();
+
+			xvel = Random.Range (-10, 10);
+			yvel = Random.Range (-10, 10);
+			zvel = Random.Range (-10, 10);
+			forceVector = new Vector3 (xvel, yvel, zvel);
+			rb.AddRelativeForce(forceVector,ForceMode.VelocityChange);
+
+			xrot = Random.Range (-10, 10);
+			yrot = Random.Range (-10, 10);
+			zrot = Random.Range (-10, 10);
+			mytoDirection = new Vector3 (xrot, yrot, zrot);
+			rb.rotation.SetFromToRotation (rb.transform.forward, mytoDirection);
+
+			xtorq = Random.Range (-2, 2);
+			ytorq = Random.Range (-2, 2);
+			ztorq = Random.Range (-2, 2);
+			torqueVector = new Vector3 (xtorq, ytorq, ztorq);
+			rb.AddRelativeTorque (torqueVector, ForceMode.VelocityChange);
+		}
+		//myRock4
+		for (var x = 0; x < 1000; x++) {
+
+			xloc = Random.Range (-200, 1300);
+			yloc = Random.Range (-200, 200);
+			zloc = Random.Range (-200, 1700);
+			locVector = new Vector3 (xloc, yloc, zloc);
+			thisRock = (Transform)Instantiate (myRock4, locVector, Quaternion.identity);
+			rb = thisRock.GetComponent<Rigidbody> ();
+
+			xvel = Random.Range (-10, 10);
+			yvel = Random.Range (-10, 10);
+			zvel = Random.Range (-10, 10);
+			forceVector = new Vector3 (xvel, yvel, zvel);
+			rb.AddRelativeForce(forceVector,ForceMode.VelocityChange);
+
+			xrot = Random.Range (-10, 10);
+			yrot = Random.Range (-10, 10);
+			zrot = Random.Range (-10, 10);
+			mytoDirection = new Vector3 (xrot, yrot, zrot);
+			rb.rotation.SetFromToRotation (rb.transform.forward, mytoDirection);
+
+			xtorq = Random.Range (-2, 2);
+			ytorq = Random.Range (-2, 2);
+			ztorq = Random.Range (-2, 2);
+			torqueVector = new Vector3 (xtorq, ytorq, ztorq);
+			rb.AddRelativeTorque (torqueVector, ForceMode.VelocityChange);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/JetControl2.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class JetControl2 : MonoBehaviour {
+
+	//Low Speed Factors
+	public float shipAngularDrag;
+	public float torqueFactor;
+	public float thrustFactor;
+	public float maxTurnFactor; //In degrees (0 to 90)
+
+	//High Speed Factors
+	public float shipAngularDragHighSpeed;
+	public float torqueFactorHighSpeed;
+	public float thrustFactorHighSpeed;
+
+	//Other Public Parameters
+	public Text displayText;
+
+	//Private variables
+	private Rigidbody rb;
+	private float shipspeed;
+	private Vector3 rotationVector;
+	private Vector3 thrustVector;
+	private float speedchange;
+	private Vector3 velVector;
+	private Vector3 towardVectorLocal;
+	private Vector3 towardVectorGlobal;
+	private Vector3 towardVectorGlobalAtMag;
+	private Vector3 rotatedVelVector;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		rb.AddRelativeForce (new Vector3(0,0,5.0f),ForceMode.VelocityChange);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		// Get joystick values
+
+		float moveRoll = Input.GetAxis ("Roll"); 
+		float movePitch = Input.GetAxis ("Pitch");
+		float moveThrust = Input.GetAxis ("Thrust");
+		bool rocketStop = Input.GetKey(KeyCode.Y);
+
+		//General Algorithm
+		// 1. At low speeds treat roll and pitch as merely damped rotations (DONE)
+		//	1.1 Add thrust in local z direction. (DONE)
+		// 2. At higher speeds treat roll and pitch as thursts in the x and y direction (DONE)
+		//	2.1 Rotate ship to be facing in direction of global velocity vector (DONE)
+		//		2.1.1 Add torque based on cross product between velocity (global) and ship forward (in global) (DONE)
+		//	2.2 Apply forward thrust force (local z) (DONE)
+		// 	2.3. Apply turning force (DONE)
+		//		2.2.1 Build desired velocity change vector (local) (DONE)
+		//			2.2.1.c magnitude less than speed (DONE)
+		//			2.2.1.d scaled in x and y based on moveRoll and movePitch values (DONE)
+
+		//Apply ship manuvering
+
+		shipspeed =  Vector3.Magnitude (rb.velocity); //Validated
+
+		if (shipspeed < 0.0001f) {
+			rb.angularDrag = shipAngularDrag; //Validated
+			rotationVector = torqueFactor * new Vector3 (movePitch, moveRoll, 0); //Validated
+			rb.AddRelativeTorque (rotationVector); //Validated
+			rb.AddRelativeForce (thrustFactor * Vector3.forward * moveThrust); //Validated
+		} else {
+			rb.angularDrag = shipAngularDragHighSpeed; //Validated
+			rotationVector = torqueFactorHighSpeed * Vector3.Cross (Vector3.Normalize (rb.velocity), Vector3.Normalize (rb.transform.forward)); //Validated
+			rb.AddTorque (-1*rotationVector); //Validated
+			rb.AddRelativeForce (thrustFactorHighSpeed * Vector3.forward * moveThrust); //Validated
+
+			// Code below handles changing velocity vector
+			// ERROR - ship speed is changing with a change in direction.
+			shipspeed = Vector3.Magnitude (rb.velocity);
+			towardVectorLocal = new Vector3 (moveRoll, movePitch, 0);
+			towardVectorGlobal = transform.TransformVector (towardVectorLocal) / 8; //8 neccessary to accomodate ship scaling.
+			towardVectorGlobalAtMag = shipspeed * Vector3.Normalize (towardVectorGlobal);
+			rotatedVelVector = Vector3.RotateTowards (rb.velocity, towardVectorGlobalAtMag, 1.570796f * maxTurnFactor/90.0f, 0.0f);
+			thrustVector = rotatedVelVector - rb.velocity;
+			rb.AddForce (thrustVector);
+		}
+
+		//Add "all stop" function
+		if (rocketStop) {
+			rb.AddForce (-1 * rb.velocity, ForceMode.VelocityChange);
+		}
+
+		// Display Information at run-time
+		//displayText.text = "moveThrust: " + moveThrust.ToString() + " moveRoll: " + moveRoll.ToString() + " Speed: "+ Vector3.Magnitude(rb.velocity).ToString();
+		//displayText.text = "moveThrust: " + moveThrust.ToString() + "  rotationVector: " + rotationVector.ToString() + "  Speed: "+ Vector3.Magnitude(rb.velocity).ToString();
+		//displayText.text = "moveThrust: " + moveThrust.ToString() + "  Speed: "+ Vector3.Magnitude(rb.velocity).ToString();
+		//displayText.text = "moveThrust: " + moveThrust.ToString() + "  speedchange: "+ speedchange.ToString() + "  Speed: "+ Vector3.Magnitude(rb.velocity).ToString();
+		//displayText.text = "shipspeed " + shipspeed.ToString() + "  thrustVector "+ thrustVector.ToString() + " mag.thrustVector: " + Vector3.Magnitude(thrustVector).ToString();
+		//displayText.text = "shipspeed " + shipspeed.ToString () + "  rotatedVelVector: " + rotatedVelVector.ToString () + " mag.thrustVector: " + Vector3.Magnitude(thrustVector).ToString() + " mag.towardVector: " + Vector3.Magnitude(towardVectorGlobal).ToString();
+		displayText.text = "shipspeed: " + shipspeed.ToString () +
+		" towardVectorLocal: " + towardVectorLocal.ToString () +
+		" towardVectorGlobal: " + towardVectorGlobal.ToString () +
+		" towardVectorGlobalAtMag: " + towardVectorGlobalAtMag.ToString () +
+		" rotatedVector: " + rotatedVelVector.ToString () +
+		" thrustVector: " + thrustVector.ToString();
+
+		// Check for application quit
+
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			Application.Quit();
+		}
+	}
+}
